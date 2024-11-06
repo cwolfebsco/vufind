70c041a4c29389f051047dd6d9f4f6503eabdd03
Claus Wolf <cwolf@ebsco.com>
2024-11-06 11:30:43 +0100

    Testing Debug
    Testing Debug - to be removed prior to go live

--- module/VuFindSearch/src/VuFindSearch/Backend/EDS/Connector.php
+++ module/VuFindSearch/src/VuFindSearch/Backend/EDS/Connector.php
@@ -101,6 +101,7 @@ class Connector extends Base implements LoggerAwareInterface
         $cacheable = true
     ) {
         $this->debug("{$method}: {$baseUrl}?{$queryString}");
+        $this->debug("Headers: ".$this->varDump($headers));
 
         $this->client->resetParameters();
 
