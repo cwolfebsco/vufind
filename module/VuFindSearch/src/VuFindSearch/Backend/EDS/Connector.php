fdbc032599544b61a80f7d632e0772ce0390c61b
Claus Wolf <cwolf@ebsco.com>
2024-11-12 14:17:43 +0100

    fix: remove debug of headers
    this was useful during testing, but is likely not useful during production debug

--- module/VuFindSearch/src/VuFindSearch/Backend/EDS/Connector.php
+++ module/VuFindSearch/src/VuFindSearch/Backend/EDS/Connector.php
@@ -101,7 +101,6 @@ class Connector extends Base implements LoggerAwareInterface
         $cacheable = true
     ) {
         $this->debug("{$method}: {$baseUrl}?{$queryString}");
-        $this->debug($this->varDump($headers));
         $this->client->resetParameters();
 
         $this->client->setHeaders($headers);
